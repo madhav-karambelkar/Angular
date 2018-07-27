6daf080ba6a2324507f8050124103ab0f7bd5ca0
Madhav Karambelkar <Madhav Karambelkar>
2018-07-27 11:28:36 +0530

    Assignment Properly p-1 & p-2 .. only p-3 left

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,10 +5,82 @@ import{ DishDetail } from '../shared/dishdetail';
 
 const DISHDETAILS: DishDetail[] = [
 					   {
-							details:'Detail Works!',
-							comments: 'Coments Works!'
+							
+							details:  [
+									       {
+									           rating: 5,
+									           comment: 'Imagine all the eatables, living in conFusion!',
+									           author: 'John Lemon',
+									           date: '2012-10-16T17:57:28.556094Z'
+									       },
+									       {
+									           rating: 4,
+									           comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+									           author: 'Paul McVites',
+									           date: '2014-09-05T17:57:28.556094Z'
+									       },
+									       {
+									           rating: 3,
+									           comment: 'Eat it, just eat it!',
+									           author: 'Michael Jaikishan',
+									           date: '2015-02-13T17:57:28.556094Z'
+									       },
+									       {
+									           rating: 4,
+									           comment: 'Ultimate, Reaching for the stars!',
+									           author: 'Ringo Starry',
+									           date: '2013-12-02T17:57:28.556094Z'
+									       },
+									       {
+									           rating: 2,
+									           comment: 'It\'s your birthday, we\'re gonna party!',
+									           author: '25 Cent',
+									           date: '2011-12-02T17:57:28.556094Z'
+									       }
+				   						]
 					   }
 					 ];
+const DISH =   { 
+				  name: 'Uthappizza',
+				  image: '/assets/images/uthappizza.png',
+				  category: 'mains',
+				  label: 'Hot',
+				  price: '4.99',
+				  // tslint:disable-next-line:max-line-length
+				  description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+				  comments: [
+						       {
+						           rating: 5,
+						           comment: 'Imagine all the eatables, living in conFusion!',
+						           author: 'John Lemon',
+						           date: '2012-10-16T17:57:28.556094Z'
+						       },
+						       {
+						           rating: 4,
+						           comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+						           author: 'Paul McVites',
+						           date: '2014-09-05T17:57:28.556094Z'
+						       },
+						       {
+						           rating: 3,
+						           comment: 'Eat it, just eat it!',
+						           author: 'Michael Jaikishan',
+						           date: '2015-02-13T17:57:28.556094Z'
+						       },
+						       {
+						           rating: 4,
+						           comment: 'Ultimate, Reaching for the stars!',
+						           author: 'Ringo Starry',
+						           date: '2013-12-02T17:57:28.556094Z'
+						       },
+						       {
+						           rating: 2,
+						           comment: 'It\'s your birthday, we\'re gonna party!',
+						           author: '25 Cent',
+						           date: '2011-12-02T17:57:28.556094Z'
+						       }
+				   			]
+				};					 
 const DISHES: Dish[] =  [
 							{
 								name:'Uthapizza',
@@ -17,6 +89,7 @@ const DISHES: Dish[] =  [
 								label:'Hot',
 								price:'4.99',
 								description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped  		with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and 		Buffalo Paneer.'
+
 							},
 
 							{
@@ -62,6 +135,9 @@ export class MenuComponent implements OnInit {
 	dishdetails=DISHDETAILS;
 
 	selectedDishDetail: DishDetail = DISHDETAILS[0];
+
+	
+
 	
 	constructor() { }
 
--- src/app/shared/dishdetail.ts
+++ src/app/shared/dishdetail.ts
@@ -1,5 +1,5 @@
 export class DishDetail
 {
-	details= string;
-	comments= string;
+	details: string[];
+	comments: string;
 }
