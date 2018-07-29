ae6ae67a1e3b5de1bcbebdce2e06aca537fece43
Madhav Karambelkar <Madhav Karambelkar>
2018-07-29 17:57:21 +0530

    Single Page App Part-1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,5 +11,7 @@ export const routes : Routes = [
 
 									{ path :'menu', component : MenuComponent },
 
+									{ path : 'contactus' , component : ContactComponent },
+
 									{ path: '' , redirectTo: '/home' , pathMatch: 'full' }
 							   ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -49,7 +50,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService, PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
